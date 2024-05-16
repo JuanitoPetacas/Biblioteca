3d5fe6d4fdead5d78540521d7048c164dc49ccf3
narva <alexnarvaez981@gmail.com>
2024-05-16 07:42:35 -0500

    inicializacion de diseño funcionario

---
+++ assets/js/pepe.js
@@ -0,0 +1,11 @@
+let btn= document.getElementById('sidebarToggle')
+let title = document.getElementById('title')
+
+btn.addEventListener('click',function(){
+
+    title.hidden
+    if(title.hidden == true){
+        title.hidden = false
+    }
+
+})
